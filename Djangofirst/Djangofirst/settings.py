8c4525bcdac7a603a02696cd69d1b8fff01ede60
Nelli Nu <alina.din363@gmail.com>
2024-09-14 19:47:13 +0500

    finally

--- Djangofirst/Djangofirst/settings.py
+++ Djangofirst/Djangofirst/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*0)_y*7dr&0@8+a=ztvgom3ic(^0i&=u(*86n@cjghvs=5$w%_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
@@ -120,17 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR , 'static')
-]
-
-
 MEDIA_URL= 'media/'
 
+STATIC_ROOT = BASE_DIR/ 'assets'
+MEDIA_ROOT = BASE_DIR/ 'media'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_DIRS =[
+   BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
